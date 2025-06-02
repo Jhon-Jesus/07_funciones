fcbb2a487bbcda2557f7113ed6bac8c753184fa3
Jhon-Jesus <jhuarcayac@unjbg.edu.pe>
2025-06-02 18:16:35 -0500

    funcion9.cpp en bruto

---
+++ funcion9.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+float factorial (float n);
+
+int main(){
+    int n;
+    float prod, suma=0;
+    cout<<"Ingrese n: ";
+    cin>>n;
+
+    prod=0.2+0.3;
+    for (int i=1;i<=n;i++){
+    prod=factorial(i)/(2*i);
+    suma=suma+prod;
+
+
+    }
+
+    cout<<"\nLa suma es: "<<suma;
+return 0;
+}
+
+float factorial (float n){
+float f=1;
+
+for (float i=2;i<=n;i++){
+    f=f*i;
+
+}
+return f;
+}
