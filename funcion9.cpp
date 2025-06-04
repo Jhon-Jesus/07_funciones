e992cab6d128978de014478d4aa25683f857905e
Jhon-Jesus <jhuarcayac@unjbg.edu.pe>
2025-06-03 22:45:24 -0500

    Pequeñas optimizaciones y remocion de residuos en funcion9.cpp

--- funcion9.cpp
+++ funcion9.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
-float factorial (float n);
+float factorial (int n);
 
 int main(){
     int n;
     float prod, suma=0;
-    cout<<"Ingrese n: ";
+    cout<<"Ingrese el valor de (n): ";
     cin>>n;
 
-    prod=0.2+0.3;
     for (int i=1;i<=n;i++){
     prod=factorial(i)/(2*i);
     suma=suma+prod;
@@ -16,14 +15,14 @@ int main(){
 
     }
 
-    cout<<"\nLa suma es: "<<suma;
+    cout<<"\nLa suma de la serie es: "<<suma;
 return 0;
 }
 
-float factorial (float n){
-float f=1;
+float factorial (int n){
+int f=1;
 
-for (float i=2;i<=n;i++){
+for (int i=2;i<=n;i++){
     f=f*i;
 
 }
